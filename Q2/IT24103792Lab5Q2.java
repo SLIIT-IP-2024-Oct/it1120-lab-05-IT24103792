f1f66deb3f2c36747c0218f21948e9e1763627d6
IT24103792 <IT24103792@my.sliit.lk>
2024-12-12 13:06:08 +0530

    Add files via upload

---
+++ Q2/IT24103792Lab5Q2.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+        public class IT24103792Lab5Q2{
+
+    public static void main(String []args) {
+
+   Scanner input=new Scanner(System.in);
+
+  int newMember;
+  System.out.println("Enter the number of new members introduced ;");
+  newMember=input.nextInt();
+
+  if(0>newMember) {
+  System.out.println("Input must be a number 0 or greater");
+}
+ else {
+  String prize;
+
+ switch(newMember) {
+
+ case 0 : 
+  prize ="No price";
+  break;
+
+ case 1 :
+  prize ="Pen";
+  break;
+
+ case 2 :
+  prize ="Umbrella";
+  break;
+
+ case 3 :
+  prize ="Bag";
+  break;
+
+ case 4 :
+  prize ="Travelling chair";
+  break;
+
+ default :
+  prize ="Headphone";
+}
+
+  System.out.println("Price is a :" +prize);
+}
+  
+
+}
+ }
